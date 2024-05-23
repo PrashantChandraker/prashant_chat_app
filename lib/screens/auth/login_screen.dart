9b5921bb38ef6f9fe2eb6625ad8dd35cf0e54f51
PrashantChandraker <prashantchandraker1@gmail.com>
2024-05-23 10:27:44 +0530

    chat app logo animation - bottom to top

--- lib/screens/auth/login_screen.dart
+++ lib/screens/auth/login_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:prashant_chat_app/screens/home_screen.dart';
 
 import '../../main.dart';
 
@@ -10,43 +11,79 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  bool _isAnimate = false;
+
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(Duration(milliseconds: 500), () {
+      setState(() {
+        _isAnimate = true;
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     mq = MediaQuery.of(context).size;
     return Scaffold(
-        appBar: AppBar(
-          automaticallyImplyLeading: false,
-          title: Text(
-            'Welcome to  P  Chat',
-          ),
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        title: Text(
+          'Welcome to  P  Chat',
         ),
-        body: Stack(
-          children: [
-            Positioned(
-                top: mq.height * 0.15,
-                left: mq.width * 0.25,
-                width: mq.width * 0.5,
-                child: Image.asset('assets/images/launcher_icon.png'),),
-                Positioned(
-                  bottom: mq.height * 0.15 ,
-                
-                left: mq.width * 0.05,
-                width: mq.width * 0.9,
-                height:  mq.height * 0.07,
-                child: ElevatedButton.icon(onPressed: (){},
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.green,
-                  shape: StadiumBorder(),
-                  elevation: 5,
-                ),
-                icon: Image.asset('assets/logo/google_logo.png', height: mq.height * 0.05,) ,
-                 label: RichText(text: TextSpan(
+      ),
+      body: Stack(
+        children: [
+          AnimatedPositioned(
+            bottom: _isAnimate ? mq.height * 0.5 : -mq.width * .5,
+            left: mq.width * 0.25,
+            width: mq.width * 0.5,
+            duration: Duration(seconds: 1),
+            child: Image.asset('assets/images/launcher_icon.png'),
+          ),
+          // google lgin button
+          Positioned(
+            bottom: mq.height * 0.15,
+            left: mq.width * 0.05,
+            width: mq.width * 0.9,
+            height: mq.height * 0.07,
+            child: ElevatedButton.icon(
+              onPressed: () {
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(
+                    builder: (_) => HomeScreen(),
+                  ),
+                );
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.green,
+                shape: StadiumBorder(),
+                elevation: 5,
+              ),
+              //google icon
+              icon: Image.asset(
+                'assets/logo/google_logo.png',
+                height: mq.height * 0.05,
+              ),
+
+              //login with google label
+              label: RichText(
+                text: TextSpan(
                   style: TextStyle(color: Colors.black, fontSize: 20),
                   children: [
-                  TextSpan(text: 'SignIn with '),
-                  TextSpan(text: 'Google', style: TextStyle(fontWeight: FontWeight.w500)),
-                 ]),),),),
-          ],
-        ));
+                    TextSpan(text: 'Login with '),
+                    TextSpan(
+                        text: 'Google',
+                        style: TextStyle(fontWeight: FontWeight.w500)),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
