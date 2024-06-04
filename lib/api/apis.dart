fc120f9cc045c9aa5f174ad39c29a5dd14b9c258
PrashantChandraker <prashantchandraker1@gmail.com>
2024-06-04 14:15:24 +0530

    function for checking user exists or not , if not then created user

--- lib/api/apis.dart
+++ lib/api/apis.dart
@@ -1,12 +1,45 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:prashant_chat_app/models/chat_user.dart';
 
-class APIs{
+class APIs {
   //for authentication
   static FirebaseAuth auth = FirebaseAuth.instance;
 
   // for accesing cloud firestore database
   static FirebaseFirestore firestore = FirebaseFirestore.instance;
 
+  // to return current user
+  static User get user => auth.currentUser!;
 
+  // for checking if user exsits or not
+  static Future<bool> userExists() async {
+    return (await firestore
+            .collection('users')
+            .doc(user.uid)
+            .get())
+        .exists;
+  }
+
+  // for creating a new user
+  static Future<void> createUser() async {
+
+    final time = DateTime.now().millisecondsSinceEpoch.toString();
+    final chatUser = ChatUser(
+      // getting the data names from debug console which we are getting from print method in "login.dart"
+        image: user.photoURL.toString(),
+        name: user.displayName.toString(),
+        about: "Hey i am usin PC Chat",
+        createdAt: time,
+        isOnline: false,
+        lastActive: time,
+        id: user.uid,
+        email: user.email.toString(),
+        pushToken: '');
+    return await firestore
+            .collection('users')
+            .doc(user.uid)
+            .set(chatUser.toJson());
+        
+  }
 }
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,8 +1,10 @@
-import 'dart:convert';
+
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:prashant_chat_app/api/apis.dart';
 import 'package:prashant_chat_app/models/chat_user.dart';
 import 'package:prashant_chat_app/widgets/chat_user_card.dart';
 
@@ -59,8 +61,8 @@ class _HomeScreenState extends State<HomeScreen> {
         backgroundColor: Color(0xff5ff1f5),
         onPressed: () async {
           // trial to check logout function is working or not
-          // await APIs.auth.signOut();
-          // await GoogleSignIn().signOut();
+          await APIs.auth.signOut();
+          await GoogleSignIn().signOut();
           setState(() {});
         },
         child: Icon(
@@ -71,7 +73,7 @@ class _HomeScreenState extends State<HomeScreen> {
       ),
       body: StreamBuilder(
         // This stream will take data from Firestore collection and it will give data to the ListView builder
-        stream: FirebaseFirestore.instance.collection('uses').snapshots(),
+        stream: FirebaseFirestore.instance.collection('users').snapshots(),
         builder: (context, snapshot) {
           switch (snapshot.connectionState) {
             // if the data is loading
