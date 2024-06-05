a5f0f0d1ce80b9c27de5e25f2406ac605ca29a82
PrashantChandraker <prashantchandraker1@gmail.com>
2024-06-05 17:37:23 +0530

    hiding keyboard if we tap anywhere

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -21,146 +21,150 @@ class ProfileScreen extends StatefulWidget {
 class _ProfileScreenState extends State<ProfileScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        title: Text(
-          'My  Profile',
-        ),
-        actions: [
-          IconButton(
-            splashColor: Colors.red,
-            tooltip: 'Logout',
-            autofocus: true,
-            onPressed: () async {
-              // for showing progress dialog
-              Dialogs.showProgressBar(context);
-
-              // sign out from app
-              await APIs.auth.signOut().then((value) async {
-                await GoogleSignIn().signOut().then((value) {
-                  // for hiding progress dialog
-                  Navigator.pop(context);
-
-                  // for moving to home screen
-                  Navigator.pop(context);
-
-                  // replacing home screen with login screen
-                  Navigator.pushReplacement(context,
-                      MaterialPageRoute(builder: (_) => LoginScreen()));
+    return GestureDetector(
+      // for hiding keyboard when tap other place
+      onTap: ()=> FocusScope.of(context).unfocus(),
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          title: Text(
+            'My  Profile',
+          ),
+          actions: [
+            IconButton(
+              splashColor: Colors.red,
+              tooltip: 'Logout',
+              autofocus: true,
+              onPressed: () async {
+                // for showing progress dialog
+                Dialogs.showProgressBar(context);
+      
+                // sign out from app
+                await APIs.auth.signOut().then((value) async {
+                  await GoogleSignIn().signOut().then((value) {
+                    // for hiding progress dialog
+                    Navigator.pop(context);
+      
+                    // for moving to home screen
+                    Navigator.pop(context);
+      
+                    // replacing home screen with login screen
+                    Navigator.pushReplacement(context,
+                        MaterialPageRoute(builder: (_) => LoginScreen()));
+                  });
                 });
-              });
-
-              // setState(() {});
-              Navigator.pushReplacement(
-                  context, MaterialPageRoute(builder: (_) => LoginScreen()));
-            },
-            icon: Icon(
-              Icons.logout,
-            ),
-          ),
-        ],
-        flexibleSpace: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-              colors: <Color>[Color(0xff5cfaad), Color(0xff5ff1f5)],
-            ),
-          ),
-        ),
-      ),
-      floatingActionButton: FloatingActionButton.extended(
-        elevation: 10,
-        backgroundColor: Colors.amber,
-        onPressed: () {},
-        icon: Icon(
-          Icons.edit,
-          color: Colors.black,
-          size: 25,
-        ),
-        label: Text(
-          'Update',
-          style: TextStyle(color: Colors.black, fontSize: 18),
-        ),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 15),
-        child: Column(
-          children: [
-            SizedBox(
-              width: mq.width,
-              height: mq.height * 0.03,
-            ),
-            Stack(
-              children: [
-                ClipRRect(
-                  borderRadius: BorderRadius.circular(
-                      mq.height * 0.1), // passing the half height
-                  child: CachedNetworkImage(
-                    width: mq.height * 0.15,
-                    height: mq.height * 0.15,
-                    fit: BoxFit.contain,
-                    imageUrl: widget.user.image,
-                    // placeholder: (context, url) => CircularProgressIndicator(),
-                    errorWidget: (context, url, error) => CircleAvatar(
-                      child: Icon(CupertinoIcons.person),
-                    ),
-                  ),
-                ),
-                Positioned(
-                  bottom: 0,
-                  left: 70,
-                  child: MaterialButton(
-                    elevation: 2,
-                    shape: CircleBorder(),
-                    color: Colors.amber,
-                    onPressed: () {},
-                    child: Icon(
-                      Icons.edit,
-                      size: 20,
-                    ),
-                  ),
-                )
-              ],
-            ),
-            SizedBox(
-              height: mq.height * 0.03,
-            ),
-            Text(
-              widget.user.email,
-              style: TextStyle(color: Colors.black, fontSize: 17),
-            ),
-            SizedBox(
-              height: mq.height * 0.03,
-            ),
-            TextFormField(
-              initialValue: widget.user.name,
-              decoration: InputDecoration(
-                  prefixIcon: Icon(CupertinoIcons.person),
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10)),
-                  hintText: 'eg. Pashant Chandraker',
-                  hintStyle: TextStyle(color: Colors.grey.shade500),
-                  labelText: 'Name'),
-            ),
-            SizedBox(
-              height: mq.height * 0.02,
-            ),
-            TextFormField(
-              initialValue: widget.user.about,
-              decoration: InputDecoration(
-                  prefixIcon: Icon(CupertinoIcons.info_circle),
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10)),
-                  hintText: 'eg. call me anti-social but dont call me',
-                  hintStyle: TextStyle(color: Colors.grey.shade500),
-                  labelText: 'Status'),
-            ),
-            SizedBox(
-              height: mq.height * 0.03,
+      
+                // setState(() {});
+                Navigator.pushReplacement(
+                    context, MaterialPageRoute(builder: (_) => LoginScreen()));
+              },
+              icon: Icon(
+                Icons.logout,
+              ),
             ),
           ],
+          flexibleSpace: Container(
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+                colors: <Color>[Color(0xff5cfaad), Color(0xff5ff1f5)],
+              ),
+            ),
+          ),
+        ),
+        floatingActionButton: FloatingActionButton.extended(
+          elevation: 10,
+          backgroundColor: Colors.amber,
+          onPressed: () {},
+          icon: Icon(
+            Icons.edit,
+            color: Colors.black,
+            size: 25,
+          ),
+          label: Text(
+            'Update',
+            style: TextStyle(color: Colors.black, fontSize: 18),
+          ),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 15),
+          child: Column(
+            children: [
+              SizedBox(
+                width: mq.width,
+                height: mq.height * 0.03,
+              ),
+              Stack(
+                children: [
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(
+                        mq.height * 0.1), // passing the half height
+                    child: CachedNetworkImage(
+                      width: mq.height * 0.15,
+                      height: mq.height * 0.15,
+                      fit: BoxFit.contain,
+                      imageUrl: widget.user.image,
+                      // placeholder: (context, url) => CircularProgressIndicator(),
+                      errorWidget: (context, url, error) => CircleAvatar(
+                        child: Icon(CupertinoIcons.person),
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    bottom: 0,
+                    left: 70,
+                    child: MaterialButton(
+                      elevation: 2,
+                      shape: CircleBorder(),
+                      color: Colors.amber,
+                      onPressed: () {},
+                      child: Icon(
+                        Icons.edit,
+                        size: 20,
+                      ),
+                    ),
+                  )
+                ],
+              ),
+              SizedBox(
+                height: mq.height * 0.03,
+              ),
+              Text(
+                widget.user.email,
+                style: TextStyle(color: Colors.black, fontSize: 17),
+              ),
+              SizedBox(
+                height: mq.height * 0.03,
+              ),
+              TextFormField(
+                initialValue: widget.user.name,
+                decoration: InputDecoration(
+                    prefixIcon: Icon(CupertinoIcons.person),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10)),
+                    hintText: 'eg. Pashant Chandraker',
+                    hintStyle: TextStyle(color: Colors.grey.shade500),
+                    labelText: 'Name'),
+              ),
+              SizedBox(
+                height: mq.height * 0.02,
+              ),
+              TextFormField(
+                initialValue: widget.user.about,
+                decoration: InputDecoration(
+                    prefixIcon: Icon(CupertinoIcons.info_circle),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10)),
+                    hintText: 'eg. call me anti-social but dont call me',
+                    hintStyle: TextStyle(color: Colors.grey.shade500),
+                    labelText: 'Status'),
+              ),
+              SizedBox(
+                height: mq.height * 0.03,
+              ),
+            ],
+          ),
         ),
       ),
     );
