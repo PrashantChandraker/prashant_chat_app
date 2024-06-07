3a1d45877e01d48dd83c8f62398aa82a876c3694
PrashantChandraker <prashantchandraker1@gmail.com>
2024-06-07 09:52:35 +0530

    appbar of chat screen

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,5 +1,7 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:prashant_chat_app/main.dart';
 import 'package:prashant_chat_app/models/chat_user.dart';
 
 class ChatScreen extends StatefulWidget {
@@ -13,6 +15,78 @@ class ChatScreen extends StatefulWidget {
 class _ChatScreenState extends State<ChatScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          automaticallyImplyLeading: false,
+          flexibleSpace: _appbar(),
+        ),
+      ),
+    );
+  }
+
+  Widget _appbar() {
+    return InkWell(
+      onTap: () {
+        
+      },
+      child: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors: <Color>[Color(0xff5cfaad), Color(0xff5ff1f5)],
+          ),
+        ),
+        child: Row(
+          children: [
+            //back button
+            IconButton(onPressed: () {
+              Navigator.pop(context);
+            }, icon: Icon(Icons.arrow_back_ios)),
+            //user profile picture
+            ClipRRect(
+              borderRadius: BorderRadius.circular(
+                  mq.height * 0.3), // passing the half height
+              child: CachedNetworkImage(
+                width: mq.height * 0.050,
+                height: mq.height * 0.050,
+                imageUrl: widget.user.image,
+                // placeholder: (context, url) => CircularProgressIndicator(),
+                errorWidget: (context, url, error) => CircleAvatar(
+                  child: Icon(CupertinoIcons.person),
+                ),
+              ),
+            ),
+            SizedBox(
+              width: 15,
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                //username
+                Text(
+                  widget.user.name,
+                  style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.black87,
+                      fontWeight: FontWeight.w500),
+                ),
+                SizedBox(height: 3,),
+                //lastseen
+                Text(
+                  'Last seen not avialable',
+                  style: TextStyle(
+                      fontSize: 12,
+                      color: Colors.black54,
+                      fontWeight: FontWeight.w600),
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
